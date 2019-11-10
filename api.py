6689c9d72a3e9f50755f02cc9f3a5821b9b04898
cafeamericano <48388789+cafeamericano@users.noreply.github.com>
2019-11-10 15:19:53 -0500

    Attempting to solve port issue on Heroku

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import flask
+import os
 
 app = flask.Flask(__name__)
 
@@ -18,4 +19,7 @@ def sendHTMLFile():
 def sendPNGFile():
     return flask.current_app.send_static_file('catnose.png')
 
-app.run()
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
