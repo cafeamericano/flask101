50a9edc3998e0d455e022ff8128c0e492620b53a
cafeamericano <48388789+cafeamericano@users.noreply.github.com>
2019-11-10 14:02:16 -0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,17 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def welcome():
+    return "<h1>Hello World</h1><p>Sent to you by a server running Flask.</p>"
+
+@app.route('/json', methods=['GET'])
+def makeJSON():
+    return flask.jsonify(name='Matthew', favoriteColor='Orage')
+
+@app.route('/htmlfile', methods=['GET'])
+def sendHTMLFile():
+    return flask.current_app.send_static_file('index.html')
+
+app.run(port=9000)
